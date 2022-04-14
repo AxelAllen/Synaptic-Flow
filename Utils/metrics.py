96d3ef89252836dc9f545a0c17922e055a8dbac6
Axel Allen <buzzlightyear@pop-os.localdomain>
2022-04-14 18:48:33 +0300

    fixed imports

--- Utils/metrics.py
+++ Utils/metrics.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 import pandas as pd
-from prune import *
 from Utils.generator import prunable
 
 def global_sparsity(model, prune_bias=False):
@@ -41,8 +40,8 @@ def summary(model, scores):
             score_var = score.var()
             score_sum = score.sum()
             score_abs_mean = np.abs(score).mean()
-            score_abs_var  = np.abs(score).var()
-            score_abs_sum  = np.abs(score).sum()
+            score_abs_var = np.abs(score).var()
+            score_abs_sum = np.abs(score).sum()
             rows.append([name, pname, sparsity, np.prod(shape), shape,
                         score_mean, score_var, score_sum,
                         score_abs_mean, score_abs_var, score_abs_sum,
@@ -50,40 +49,4 @@ def summary(model, scores):
     columns = ['module', 'param', 'sparsity', 'size', 'shape', 'score mean', 'score variance',
                'score sum', 'score abs mean', 'score abs variance', 'score abs sum', 'prunable']
     return pd.DataFrame(rows, columns=columns)
-
-
-# def conservation(model, scores, batchnorm, residual):
-#     r"""Summary of conservation results for a model.
-#     """
-#     rows = []
-#     bias_flux = 0.0
-#     mu = 0.0
-#     for name, module in reversed(list(model.named_modules())):
-#         if prunable(module, batchnorm, residual):
-#             weight_flux = 0.0
-#             for pname, param in module.named_parameters(recurse=False):
-                
-#                 # Get score
-#                 score = scores[id(param)].detach().cpu().numpy()
-                
-#                 # Adjust batchnorm bias score for mean and variance
-#                 if isinstance(module, (layers.Linear, layers.Conv2d)) and pname == "bias":
-#                     bias = param.detach().cpu().numpy()
-#                     score *= (bias - mu) / bias
-#                     mu = 0.0
-#                 if isinstance(module, (layers.BatchNorm1d, layers.BatchNorm2d)) and pname == "bias":
-#                     mu = module.running_mean.detach().cpu().numpy()
-                
-#                 # Add flux
-#                 if pname == "weight":
-#                     weight_flux += score.sum()
-#                 if pname == "bias":
-#                     bias_flux += score.sum()
-#             layer_flux = weight_flux
-#             if not isinstance(module, (layers.Identity1d, layers.Identity2d)):
-#                 layer_flux += bias_flux
-#             rows.append([name, layer_flux])
-#     columns = ['module', 'score flux']
-
-#     return pd.DataFrame(rows, columns=columns)
 
