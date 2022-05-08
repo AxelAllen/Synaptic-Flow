5e40d16e7f4d380470edc63bcbcc842b88892d51
Axel Allen <buzzlightyear@pop-os.localdomain>
2022-05-08 03:16:12 +0200

    fixed memory issue

--- prune.py
+++ prune.py
@@ -22,34 +22,30 @@ def prune_loop(model, prune_class, dataloader, loss, device, sparsity, schedule,
 
     importance_scores = None
     # Prune model
-    with torch.autograd.set_detect_anomaly(True):
-        for epoch in tqdm(range(epochs)):
-            print(f"memory allocated before score: {torch.cuda.memory_allocated(device=device)}")
-            importance_scores = pruner.score(model, dataloader, loss, device, prune_bias)
-            print(f"memory allocated after score: {torch.cuda.memory_allocated(device=device)}")
-            sparse = sparsity
-            if schedule == 'exponential':
-                sparse = sparsity**((epoch + 1) / epochs)
-            elif schedule == 'linear':
-                sparse = 1.0 - (1.0 - sparsity)*((epoch + 1) / epochs)
-            print(f"memory allocated before params: {torch.cuda.memory_allocated(device=device)}")
-            params = []
-            for module in filter(lambda p: prunable(p), model.modules()):
-                for pname, param in module.named_parameters(recurse=False):
-                    if pname == "bias" and prune_bias is False:
-                        continue
-                    params.append((module, pname))
-            print(f"memory allocated after params and before pruning: {torch.cuda.memory_allocated(device=device)}")
-            prune_.global_unstructured(parameters=params, pruning_method=prune_method, importance_scores=importance_scores,
-                                    amount=sparse)
-            print(f"memory allocated after pruning and before remove: {torch.cuda.memory_allocated(device=device)}")
-            for module in filter(lambda p: prunable(p), model.modules()):
-                if hasattr(module, 'weight'):
-                    prune_.remove(module, "weight")
-                if hasattr(module, "bias") and prune_bias is True:
-                    prune_.remove(module, "bias")
-            print(f"memory allocated after removing: {torch.cuda.memory_allocated(device=device)}")
-    
+    # print(torch.cuda.memory_allocated(device=device))
+    # with torch.autograd.set_detect_anomaly(True):
+    for epoch in tqdm(range(epochs)):
+        importance_scores = pruner.score(model, dataloader, loss, device, prune_bias)
+        sparse = sparsity
+        if schedule == 'exponential':
+            sparse = sparsity**((epoch + 1) / epochs)
+        elif schedule == 'linear':
+            sparse = 1.0 - (1.0 - sparsity)*((epoch + 1) / epochs)
+        params = []
+        for module in filter(lambda p: prunable(p), model.modules()):
+            for pname, param in module.named_parameters(recurse=False):
+                if pname == "bias" and prune_bias is False:
+                    continue
+                params.append((module, pname))
+        prune_.global_unstructured(parameters=params, pruning_method=prune_method, importance_scores=importance_scores,
+                                amount=sparse)
+        for module in filter(lambda p: prunable(p), model.modules()):
+            if hasattr(module, 'weight'):
+                prune_.remove(module, "weight")
+            if hasattr(module, "bias") and prune_bias is True:
+                prune_.remove(module, "bias")
+
+
     # make pruning permanent
     if epochs > 0:
         ''''
